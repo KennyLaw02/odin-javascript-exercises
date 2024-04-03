99aff60d615ea18962b38ce6f02a4727e4e2b9ef
Kenneth Law <z5363506@ad.unsw.edu.au>
2024-04-03 22:26:30 +1100

    Completed 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(stringGiven, repetitions) {
+    if (repetitions < 0) {
+        return "ERROR";
+    }
+    let result = "";
+    for (let i = 0; i < repetitions; i++) {
+        result += stringGiven;
+    }
+    return result;
 };
 
 // Do not edit below this line
