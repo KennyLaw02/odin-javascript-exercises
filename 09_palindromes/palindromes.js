6f8db8e4f160bef13665526a11c1dfc3abf60f49
Kenneth Law <z5363506@ad.unsw.edu.au>
2024-04-04 20:04:06 +1100

    Completed 09_palindromes exercise using .filter method

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+function isAlphanumeric(letter) {
+    return /^[a-zA-Z0-9]+$/.test(letter);
+}
+const palindromes = function(givenString) {
+    // Remove uppercases
+    givenString = givenString.toLowerCase();
 
+    let stringArray = givenString.split("");
+    // Filter out punctuation
+    stringArray = stringArray.filter((letter) => {
+       return isAlphanumeric(letter) 
+    })
+    let reverseArray = [...stringArray];
+    reverseArray.reverse();
+    // Loop through both arrays. If we hit ANY mismatch
+    // immediately return false
+    for (let i = 0; i < stringArray.length; i++) {
+        if (reverseArray[i] !== stringArray[i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
