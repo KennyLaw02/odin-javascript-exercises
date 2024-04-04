1aa91c02de3f6060412e8667332e1115122cbf7f
Kenneth Law <z5363506@ad.unsw.edu.au>
2024-04-04 19:49:04 +1100

    Completed 08_calculator using .reduce method and recursion for the trickier problems

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(givenArray) {
+    let result = givenArray.reduce((accumulator, currVal) => {
+        return accumulator + currVal;
+    }, 0);
+    return result;
 };
 
-const multiply = function() {
-
+const multiply = function(givenArray) {
+    return givenArray.reduce((accumulator, currVal) => {
+        return accumulator * currVal;
+    });
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+    return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(start) {
+    if (start === 0) {
+        return 1;
+    }
+    return start * factorial(start - 1);
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };
