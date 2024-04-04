d700f3704c0e39ee20d2de3bcb17b09d0534edea
Kenneth Law <z5363506@ad.unsw.edu.au>
2024-04-04 20:17:28 +1100

    Completed 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (!Number.isInteger(n)) {
+        n = parseInt(n);
+    }
+    if (n < 0) {
+        return "OOPS";
+    }
+    // 0th fib should just return 0
+    if (n === 0) {
+        return 0;
+    }
+    let fibSequence = [1, 1];
+    for (let i = 0; i < n - 2; i++) {
+        fibSequence.push(
+            fibSequence[fibSequence.length - 1] + fibSequence[fibSequence.length - 2],
+        );
+    }
+    return fibSequence[n - 1];
 };
 
 // Do not edit below this line
