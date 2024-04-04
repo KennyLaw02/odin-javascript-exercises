c497fd19a05d4b96a8a5b34c658da6112bcf22e1
Kenneth Law <z5363506@ad.unsw.edu.au>
2024-04-04 19:27:21 +1100

    Completed 07_tempConversion exercise, using .toFixed method with parseFloat

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fVal) {
+    let celsiusVal = ((fVal - 32) * 5) / 9;
+    return parseFloat(celsiusVal.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusVal) {
+    let fVal = (celsiusVal * 9) / 5 + 32;
+    return parseFloat(fVal.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+    convertToCelsius,
+    convertToFahrenheit,
 };
