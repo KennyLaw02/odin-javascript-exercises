0ff3ba089b2afe4059d5a24c9a62c1b8d13ebdc7
Kenneth Law <z5363506@ad.unsw.edu.au>
2024-04-05 19:49:07 +1100

    Completed 12_findTheOldest using a clean .reduce method

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+function calcAge(person) {
+    // If person has no yearOfDeath property, they're alive
+    let deathYear = person.yearOfDeath;
+    if (!deathYear) {
+        let currYear = new Date();
+        deathYear = currYear.getFullYear();
+    }
+    return deathYear - person.yearOfBirth;
+}
+const findTheOldest = function(peopleList) {
+    let oldest = peopleList.reduce((oldestPerson, person) => {
+        return calcAge(oldestPerson) > calcAge(person) ? oldestPerson : person
+    })
+    return oldest;
 };
 
 // Do not edit below this line
